c68774704d04ee621ef977bd79366c8ecd2bda37
Philip Mark Deazeta <philip@tradegecko.com>
2018-10-16 16:09:33 +0800

    [TWEAK] Fix redirection from Lazada back to Tradegecko

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,10 +1,33 @@
 class AuthenticationsController < ApplicationController
   def callback
-    # This is to simulate the flow after Cresco has been authenticated by Lazada
-    # Save the tokens from Lazada
-    # Create Channel in TG
-    # Create webhooks in a background job
-    # redirect to install flow settings step
-    redirect_to "http://go.lvh.me:3000/integrations/shopify-install/location-setting/174"
+    case params[:provider]
+    when 'lazada'
+      # This is to simulate the flow after Cresco has been authenticated by Lazada
+      # Save the tokens from Lazada
+      # Create Channel in TG
+      # Create webhooks in a background job
+      # redirect to install flow settings step
+      redirect_to "http://go.lvh.me:3000/integrations/shopify-install/location-setting/174"
+    when 'tradegecko'
+      account = Account.find_or_create_from_omniauth(auth)
+      token = OAuth2::AccessToken.new(OAuthSession.oauth_client,
+        auth["credentials"]["token"],
+        auth["credentials"].slice("refresh_token", "expires_at")
+      )
+      account.update_from_access_token(token)
+      session[:account_id] = account.id
+      session[:user_id] = auth.uid
+      redirect_to lazada_auth_url
+    end
+  end
+
+private
+
+  def auth
+    request.env["omniauth.auth"]
+  end
+
+  def lazada_auth_url
+    "https://auth.lazada.com/oauth/authorize?response_type=code&force_auth=true&redirect_uri=#{ENV['NGROK_URL']}/auth/lazada/callback&client_id=106561"
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,8 @@
 class SessionsController < ApplicationController
   before_action :authenticate_user!, only: [:destroy]
 
-  def create
-    account = Account.find_or_create_from_omniauth(auth)
-    token = OAuth2::AccessToken.new(OAuthSession.oauth_client,
-      auth["credentials"]["token"],
-      auth["credentials"].slice("refresh_token", "expires_at")
-    )
-    account.update_from_access_token(token)
-    session[:account_id] = account.id
-    session[:user_id] = auth.uid
-    redirect_to "https://auth.lazada.com/oauth/authorize?response_type=code&force_auth=true&redirect_uri=https://crescodata.com/auth/tradegecko/callback&client_id=106561"
-  end
-
   def destroy
     clear_session
     render inline: "You have been logged out"
-  end
-
-private
-
-  def auth
-    request.env["omniauth.auth"]
   end
 end
