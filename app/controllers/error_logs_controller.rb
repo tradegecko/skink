c833c0b7ca4c303018430fcbac8457b34a2cff33
Ana Chua <anaprimawaty@gmail.com>
2018-10-17 18:35:49 +0800

    [FEATURE] Error logs controller with index, ignore and retry actions

---
+++ app/controllers/error_logs_controller.rb
@@ -0,0 +1,54 @@
+class ErrorLogsController < ApplicationController
+  before_action :verify_request
+  before_action :load_collection, only: :index
+  before_action :load_resource, only: [:ignore, :retry]
+
+  def index
+    if params[:term]
+      @error_logs = @error_logs.where("message LIKE ?", "%#{params[:term]}%")
+    end
+    if limit
+      @error_logs = @error_logs.limit(end_idx)[start_idx..end_idx]
+    end
+    render json: @error_logs.to_json
+  end
+
+  def ignore
+    @error_log.delete
+  end
+
+  def retry
+    # Add retry logic here
+    @error_log.delete
+  end
+
+private
+
+  def verify_request
+    TradegeckoVerifier.new(request).run
+  end
+
+  def load_collection
+    @error_logs = ErrorLog.where(channel_id: params[:id])
+  end
+
+  def load_resource
+    @error_log = ErrorLog.find(params[:error_id])
+  end
+
+  def limit
+    params[:limit] ? params[:limit].to_i : nil
+  end
+
+  def offset
+    params[:offset] ? params[:offset].to_i : 0
+  end
+  
+  def start_idx
+    offset * limit
+  end
+
+  def end_idx
+    start_idx + limit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'authentications#callback'
   post '/channels/:id/settings', to: 'channels#settings'
   get '/channels/:id/settings', to: 'channels#show'
+  get '/channels/:id/logs', to: 'error_logs#index'
+  get '/channels/:id/logs/:error_id/ignore', to: 'error_logs#ignore'
+  get '/channels/:id/logs/:error_id/retry', to: 'error_logs#retry'
 end
