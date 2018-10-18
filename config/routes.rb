2a3a317e22505c83f860e5165c44e86e77eab175
Ana Chua <anaprimawaty@gmail.com>
2018-10-18 12:10:11 +0800

    [ENHANCEMENT] Change ignore and retry to POST

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   post '/channels/:id/settings', to: 'channels#settings'
   get '/channels/:id/settings', to: 'channels#show'
   get '/channels/:id/logs', to: 'error_logs#index'
-  get '/channels/:id/logs/:error_id/ignore', to: 'error_logs#ignore'
-  get '/channels/:id/logs/:error_id/retry', to: 'error_logs#retry'
+  post '/channels/:id/logs/:error_id/ignore', to: 'error_logs#ignore'
+  post '/channels/:id/logs/:error_id/retry', to: 'error_logs#retry'
 end
